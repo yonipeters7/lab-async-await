5da2d16b08f25586291d6778f099d1de4729a197
David Doan <daviddoan@Davids-MacBook-Pro.local>
2025-01-02 18:37:01 -0700

    testing

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,12 +1,57 @@
-// The code below ensures that students who are using CodeGrade will get credit
-// for the code-along in Canvas; you can disregard it.
+const chai = require('chai');
+global.expect = chai.expect;
 
-require("./helpers.js");
+const fs = require('fs');
+const path = require('path');
+const { JSDOM } = require('jsdom');
+const babel = require('@babel/core');
 
-describe("", () => {
-  describe("", () => {
-    it("Test passing", () => {
-      return true;
-    });
-  });
+// Load HTML content
+const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8');
+
+// Transform JavaScript using Babel
+const { code: transformedScript } = babel.transformFileSync(
+  path.resolve(__dirname, '..', 'index.js'),
+  { presets: ['@babel/preset-env'] }
+);
+
+// Initialize JSDOM
+const dom = new JSDOM(html, {
+  runScripts: "dangerously",
+  resources: "usable"
 });
+
+//Handle fetch
+const fetchPkg = 'node_modules/whatwg-fetch/dist/fetch.umd.js';
+dom.window.eval(fs.readFileSync(fetchPkg, 'utf-8'));
+
+// Inject the transformed JavaScript into the virtual DOM
+const scriptElement = dom.window.document.createElement("script");
+scriptElement.textContent = transformedScript;
+dom.window.document.body.appendChild(scriptElement);
+
+// Expose JSDOM globals to the testing environment
+global.window = dom.window;
+global.document = dom.window.document;
+global.navigator = dom.window.navigator;
+global.HTMLElement = dom.window.HTMLElement;
+global.Node = dom.window.Node;
+global.Text = dom.window.Text;
+global.XMLHttpRequest = dom.window.XMLHttpRequest;
+
+// Sample test suite for JavaScript event handling
+describe('Asynchronous Fetching ', () => {
+  it('should fetch to external api and add information to page', async() => {
+    await new Promise(resolve => setTimeout(resolve, 200)); 
+    let postDisplay = document.querySelector("#post-list")
+    expect(postDisplay.innerHTML).to.include('sunt aut')
+    
+  })
+  it('should create an h1 and p element to add', async() => {
+    await new Promise(resolve => setTimeout(resolve, 200)); 
+    let h1 = document.querySelector("h1")
+    let p = document.querySelector("p")
+    expect(h1.textContent).to.include("sunt aut facere repellat")
+    expect(p.textContent).to.include("quia et suscipit\nsuscipit")
+  })
+})
